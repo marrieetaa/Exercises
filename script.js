b82a2d0a9289ca3d1f62526370a0b2e62dfebced
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-04-13 07:37:20 +0400

    feat: complete first lesson exercises - JavaScript basics

---
+++ script.js
@@ -0,0 +1,23 @@
+//Lesson 1 - JavaScript Basics
+//1a. Use alert(..); to display 'Good Morning' in popup
+      alert('Good Morning');
+//1b. Display your name is popup
+      alert('Mariam');
+//1c. Using math, calculate 10 + 5 in the Console
+      console.log(10 + 5);
+//1d. Calculate 20 - 5
+      console.log(20 - 5);
+//1e. Calculate 2 + 2 - 5 in the Console
+      console.log(2 + 2 - 5);
+//1f. Use document.body.innerHTML = ..; to display 'Good Morning' on the web page
+      document.body.innerHTML = 'Good Morning';
+//1g. Display your name on the web page
+      document.body.innerHTML = 'Mariam';
+//1h. You order a T-shirt for 10$, socks for 8$, and dinner plates for 20$.
+   // Use JavaScript to calculate the total cost of your order.
+      console.log(10 + 8 + 20);
+//1i. Your bank account has 100$, you spend 20$ on lunch, 50$ on dinner,
+   //and earn 200$ from your job.Calculate how much money you have.
+      console.log(100 - 20 - 50 + 200);
+//1j. Use document.body.innerHTML = ..; to make the web page blank;
+      document.body.innerHTML = '';
