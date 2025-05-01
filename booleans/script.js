ac11e171a690cab15320bd07666bb468781ab458
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-05-01 19:27:21 +0400

    docs: complete explanation of sixth lesson on Booleans and if/else statements

---
+++ booleans/script.js
@@ -0,0 +1,32 @@
+//Booleans
+console.log(3 < 5);
+console.log(5 < 3);
+
+console.log(typeof 'true');
+console.log(typeof true);
+
+console.log(5 == '5');
+console.log(5 === '5');
+console.log(5 !== 5);
+
+console.log(3 > 5 - 5);
+
+//If/else 
+if(false){
+  console.log('Hello');
+}else{
+  console.log('Bye');
+}
+
+//driveAgeCheck
+
+const age = 15;
+
+if (age >= 16) {
+   console.log('You can drive');
+}else if(age >= 14) {
+   console.log('Almost there');
+}else {
+  console.log('You can\'t drive');
+}
+
