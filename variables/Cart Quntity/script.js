b1dbe9a1df353781e46bf2b0eec1afe8db5ba08a
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-05-03 15:21:33 +0400

    feat: complete part two of sixth lesson exercises on Booleans and if/else satements

--- variables/Cart Quntity/script.js
+++ variables/Cart Quntity/script.js
@@ -1,2 +1,9 @@
 let cartQuantity = 0;
 
+//6k.Let's say cart has maximum quantity of 10. Before updating the quantity. Check if the quantity will be greater than 10:
+//-If it will, display a popup saying 'The cart is full' and don't update the quantity.
+//-Otherwise, update the quantity and console.log() it as usual.
+
+//6l.In exercises 5i-5k , we created the 'Remove from cart','-2','-3' buttons. Before updating the quantity, check if it will go below 0: 
+//-If it will, create a popup saying 'Not enough items in the cart' and don't update the quantity.
+//-Otherwise, update the quantity and console.log() it as usual.
