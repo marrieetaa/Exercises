eef1213d77075e5f82a0e8d42622b84e1ec787d7
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-05-08 16:40:09 +0400

    feat: complete explanation if seventh lesson on functions

---
+++ functions/script.js
@@ -0,0 +1,18 @@
+//functions
+/*function function1 () {
+  console.log('Hello');
+  console.log(2 + 2);
+}
+
+function1();
+function1();
+
+*/
+
+
+function calculateTax(cost, taxPercent = 0.1) {
+  console.log(cost * taxPercent);
+}
+
+calculateTax(2000, 0.2);
+calculateTax(3000);
